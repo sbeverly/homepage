d29078ed86c4e0f3239f801c2d1555e0305e1f7d
Siyan Beverly <siyan.beverly@gmail.com>
2015-06-03 14:15:32 -0400

    remove scope dependency from posts controller remove comments

--- src/app/components/blog/post.controller.js
+++ src/app/components/blog/post.controller.js
@@ -1,22 +1,14 @@
 'use strict';
 
 angular.module('homepage')
-  .controller('PostCtrl', function ($scope, $http, $stateParams, blogService) {
+  .controller('PostCtrl', function ($http, $stateParams, blogService) {
   	var self = this
 
   	self.selectedPost = null;
 
-  	// self.loadPost = function() {
-  	// 	$http.get("http://localhost:4000/api/blog_posts/" + $stateParams.id).
-  	// 		success(function(data) {
-  	// 			self.selectedPost = data
-  	// 		})
-  	// }
-
     self.loadPost = function() {
       blogService.postsPromise().then(function() {
         var post = blogService.findPost($stateParams.id)
-        console.log(post)
         self.selectedPost = post
       })
     };
