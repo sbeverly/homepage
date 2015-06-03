6e8eba2555a622a0568f18f82493e9cb2c3b0212
Siyan Beverly <siyan.beverly@gmail.com>
2015-06-03 15:51:31 -0400

    limit next and previous post buttons to available post ids

--- src/app/components/blog/post.controller.js
+++ src/app/components/blog/post.controller.js
@@ -1,15 +1,36 @@
 'use strict';
 
 angular.module('homepage')
-  .controller('PostCtrl', function ($http, $stateParams, blogService) {
+  .controller('PostCtrl', function ($stateParams, blogService, lodash) {
   	var self = this
 
-  	self.selectedPost = null;
+  	self.selectedPost = {};
+    self.postIds = [];
 
-    self.loadPost = function() {
+    self.loadPosts = function() {
       blogService.postsPromise().then(function() {
-        var post = blogService.findPost($stateParams.id)
-        self.selectedPost = post
+        self.selectedPost = blogService.findPost($stateParams.id);
+        self.postIds = lodash.pluck(blogService.getPosts(), 'id');
       })
+    };
+
+    self.nextPostId = function() {
+      var currentPostIndex = lodash.indexOf(self.postIds, self.selectedPost.id);
+
+      if(self.selectedPost.id == lodash.last(self.postIds)) {
+        return false
+      } else {
+        return self.postIds[currentPostIndex + 1]
+      }
+    };
+
+    self.previousPostId = function() {
+      var currentPostIndex = lodash.indexOf(self.postIds, self.selectedPost.id);
+
+      if(self.selectedPost.id == lodash.first(self.postIds)) {
+        return false
+      } else {
+        return self.postIds[currentPostIndex - 1]
+      }
     };
   });
