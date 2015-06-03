3a94424bfb7622231a929dd5b2b636baafd952b4
Siyan Beverly <siyan.beverly@gmail.com>
2015-06-03 14:17:39 -0400

    remove scope from blog controller, add semicolons

--- src/app/components/blog/blog.controller.js
+++ src/app/components/blog/blog.controller.js
@@ -1,23 +1,23 @@
 'use strict';
 
 angular.module('homepage')
-  .controller('BlogCtrl', function ($scope, $http, blogService) {
-  	var self = this
+  .controller('BlogCtrl', function ($http, blogService) {
+  	var self = this;
 
   	self.posts = null;
   	self.selectedPost = null;
 
   	self.getPosts = function() {
   	  blogService.postsPromise().then(function() {
-        self.posts = blogService.getPosts()
-      })
-  	}
+        self.posts = blogService.getPosts();
+      });
+  	};
 
   	self.selectPost = function(index) {
-  		self.selectedPost = index
-  	}
+  		self.selectedPost = index;
+  	};
 
   	self.clearSelectedPost = function() {
   		self.selectedPost = null;
-  	}
+  	};
   });
